57f0b096d5f932cde9ec2714fa1e752856b7f2e9
AlekGodunov <78654609+AlekGodunov@users.noreply.github.com>
2022-04-27 01:09:22 +0300

    Add

--- lib/base_case.py
+++ lib/base_case.py
@@ -1,3 +1,8 @@
+import json.decoder
+import string
+import random
+from datetime import datetime
+
 from requests import Response
 
 
@@ -9,3 +14,34 @@ class BaseCase:
     def get_header(self, response: Response, headers_name):
         assert headers_name in response.headers, f"Cannot find headers with name {headers_name} in the last response"
         return response.headers[headers_name]
+
+    def get_json_value(self, response: Response, name):
+        try:
+            response_as_dict = response.json()
+        except json.decoder.JSONDecodeError:
+            assert False, f"Response is not in JSON format. Response text is '{response.text}'"
+
+        assert name in response_as_dict, f"Response JSON doesn't have key '{name}'"
+        return response_as_dict[name]
+
+    def prepare_registration_data(self, email=None):
+        if email is None:
+            base_part = "learnqa"
+            domain = "example.com"
+            random_part = datetime.now().strftime("%m%d%Y%H%M%S")
+            email = f"{base_part}{random_part}@{domain}"
+        return {
+            'password': '123',
+            'username': 'learnqa',
+            'firstName': 'learnqa',
+            'lastName': 'learnqa',
+            'email': email
+        }
+
+    def generate_random_string(self, length):
+        letters = string.ascii_lowercase
+        rand_string = ''.join(random.choice(letters) for i in range(length))
+        return rand_string
+
+
+
